8b333f80252cfceca2ab2b86c6b309b8205ef987
i-am-shodan <6901273+i-am-shodan@users.noreply.github.com>
2024-03-19 11:45:25 +0000

    wip

--- src/arduino/net/Adafruit_USBD_NET.cpp
+++ src/arduino/net/Adafruit_USBD_NET.cpp
@@ -184,7 +184,8 @@ static err_t netif_init_cb(struct netif *netif)
 Adafruit_USBD_NET::Adafruit_USBD_NET() {
   struct netif *netif = &netif_data;
 
-  tcpip_init()
+  //tcpip_init()
+  esp_netif_init();
 
   netif->hwaddr_len = sizeof(tud_network_mac_address);
   memcpy(netif->hwaddr, tud_network_mac_address, sizeof(tud_network_mac_address));
